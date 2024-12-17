52bc041de86dc01a6f6994b7a8e04582aa32b377
benjaminmweribaya <b3njaminbaya@gmail.com>
2024-12-18 00:28:10 +0300

    feat: implement add_user functionality to create new users

--- lib/helpers.py
+++ lib/helpers.py
@@ -1,7 +1,15 @@
-# lib/helpers.py
+from lib.models import session
+from lib.models.user import User
+from lib.models.book import Book
+from lib.models.reading_progress import ReadingProgress
 
-def helper_1():
-    print("Performing useful function#1.")
+def add_user():
+    name = input("Enter your name: ")
+    email = input("Enter your email: ")
+    user = User(name=name, email=email)
+    session.add(user)
+    session.commit()
+    print("User added successfully!")
 
 
 def exit_program():
